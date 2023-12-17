1b171f24f6dd30e3515c0a76452e904b66a68979
colawithsauce <cola_with_sauce@foxmail.com>
2023-12-18 00:49:49 +0800

    Add one post

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((org-hugo-auto-set-lastmod . t)
-              (org-hugo-default-section-directory . "posts")
+((org-mode . ((org-hugo-section . "posts")
+              (org-hugo-auto-set-lastmod . t)
               (org-hugo-base-dir . "/home/colawithsauce/Projects/Hugo/"))))
